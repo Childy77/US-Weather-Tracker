b81dc034c3f92ebdf37cbf6872a18b0c0859a5fc
Ryan <ryanbairdchilders@gmail.com>
2022-12-14 11:33:46 -0800

    made changes to the JavaScript

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # US-Weather-Tracker
 
--I added the needed links to creat the webpage
+-I added the needed links to create the webpage
+
+-I created the html and css styles to the page 
+
+-I attached the API key for the weather information
+
+-I created a fetch funstion to grab the weather data to display on the page
+
+-I created a for loop to display the future forcast for every 3 hours
--- script.js
+++ script.js
@@ -5,13 +5,51 @@
 
 var APIKey = "793ec56b5b5da16e6e96c06afe923c74";
 var city;
-var inputEl = document.querySelector(".input");
 var searchBtn = document.querySelector(".search-button");
+
+
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+function citySearch(event) {
+    event.preventDefault()
+    var inputEl = document.querySelector(".input");
+    city = inputEl.value
 
-var weatherURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + APIKey;
+var weatherURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
-var forecastURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + APIKey;
 
 
 // fetch(queryURL)
+
+fetch(weatherURL)
+.then(function(data) {
+    return data.json()
+})
+.then(function(response) {
+    $(".city").text(response.name);
+    $(".current-date").text(dayjs.unix(response.dt).format("MM/DD/YYYY"));
+    $(".weather-icon").attr("src", "http://openweathermap.org/img/wn/"+ response.weather[0].icon + "@2x.png");
+    $(".temperature").text(response.main.temp);
+    $(".humidty").text(response.main.humidity);
+    $(".wind").text(response.wind.speed);
+    console.log(response);
+    
+    var forecastURL =  "https://api.openweathermap.org/data/2.5/forecast?lat="+response.coord.lat+"&lon="+response.coord.lon+"&units=imperial&appid=" + APIKey;
+    
+    fetch(forecastURL)
+    .then(function(data) {
+        return data.json()
+    })
+    .then(function(response) {
+        
+        console.log(response);
+
+        for (let i = 0; i < response.list.length; i+=8) {
+            const dayForcast = response.list[i];
+            $("#five-day").append('<div class="col days"><p class="day-1-date">'+dayjs.unix(dayForcast.dt).format("MM/DD/YYYY")+'</p><p class="day-1-icon"></p><p class="day-1-temperature">'+dayForcast.main.temp+'</p><p class="day-1-humidity">'+dayForcast.main.humidity+'</p></div>')
+            
+        }
+    })
+})
+}
+
+searchBtn.addEventListener("click", citySearch);
