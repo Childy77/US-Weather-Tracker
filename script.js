12684367c1160bf904b8bdbcc885bc44761bb2ae
Ryan <ryanbairdchilders@gmail.com>
2022-12-13 17:25:21 -0800

    updated HTML

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-var APIKey = 793ec56b5b5da16e6e96c06afe923c74;
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// Add eventlistener for button
+// fetch weather url
+// 
 
-fetch(queryURL)
+
+var APIKey = "793ec56b5b5da16e6e96c06afe923c74";
+var city;
+var inputEl = document.querySelector(".input");
+var searchBtn = document.querySelector(".search-button");
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var weatherURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + APIKey;
+
+var forecastURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + APIKey;
+
+
+// fetch(queryURL)
