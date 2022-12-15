7e1d269f8728fc97d4830cbaa4b9b19012a4545e
Ryan <ryanbairdchilders@gmail.com>
2022-12-15 09:57:41 -0800

    made changes to Javascript

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 var APIKey = "793ec56b5b5da16e6e96c06afe923c74";
 var city;
 var searchBtn = document.querySelector(".search-button");
+var savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
 
 
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
@@ -13,6 +14,7 @@ function citySearch(event) {
     event.preventDefault()
     var inputEl = document.querySelector(".input");
     city = inputEl.value
+    saveCity(city);
 
 var weatherURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
@@ -29,7 +31,7 @@ fetch(weatherURL)
     $(".current-date").text(dayjs.unix(response.dt).format("MM/DD/YYYY"));
     $(".weather-icon").attr("src", "http://openweathermap.org/img/wn/"+ response.weather[0].icon + "@2x.png");
     $(".temperature").text(response.main.temp);
-    $(".humidty").text(response.main.humidity);
+    $(".humidity").text(response.main.humidity);
     $(".wind").text(response.wind.speed);
     console.log(response);
     
@@ -42,10 +44,17 @@ fetch(weatherURL)
     .then(function(response) {
         
         console.log(response);
+        $("#five-day").empty();
 
         for (let i = 0; i < response.list.length; i+=8) {
+            
             const dayForcast = response.list[i];
-            $("#five-day").append('<div class="col days"><p class="day-1-date">'+dayjs.unix(dayForcast.dt).format("MM/DD/YYYY")+'</p><p class="day-1-icon"></p>Temp:<p class="day-1-temperature">'+dayForcast.main.temp+'</p>Humidity:<p class="day-1-humidity">'+dayForcast.main.humidity+'</p>Wind Speed:<p class="day-1-wind">'+dayForcast.wind.speed+'</p></div>')
+            var icon = "http://openweathermap.org/img/wn/"+ dayForcast.weather[0].icon + "@2x.png";
+
+            console.log("dayForcast", dayForcast);
+            console.log("icon", icon);
+
+            $("#five-day").append('<div class="col days"><p class="day-1-date">'+dayjs.unix(dayForcast.dt).format("MM/DD/YYYY")+'</p><p class="day-1-icon"><img class="weather-icon" src="'+ icon +'"></p>Temp:<p class="day-1-temperature">'+dayForcast.main.temp+'</p>Humidity:<p class="day-1-humidity">'+dayForcast.main.humidity+'</p>Wind Speed:<p class="day-1-wind">'+dayForcast.wind.speed+'</p></div>')
             
         }
     })
@@ -53,3 +62,26 @@ fetch(weatherURL)
 }
 
 searchBtn.addEventListener("click", citySearch);
+renderCityList(savedCities);
+
+
+function saveCity(city) {
+    savedCities.push(city);
+    localStorage.setItem("savedCities", JSON.stringify(savedCities))
+}
+
+function renderCityList(cityList) {
+    for (let i = 0; i < cityList.length; i++) {
+        var buttonEl = $("<button>");
+        buttonEl.text(cityList[i]);
+        // add style here, and add click event listener
+        buttonEl.click(function() {
+            $("#input").val(cityList[i]);
+            $(".search-button").click();
+        })
+        $("#city-list").append(buttonEl);
+    }
+}
+
+// add a function that will take the savedcities array and for each saved city I need a button to name them.  I will need another for loop for each button.
+
